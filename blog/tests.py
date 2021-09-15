a9e4e720a6d44e7f35acea1a30db1ec3046d3e63
Sohyun Kim <prrsh22@gmail.com>
2021-09-16 00:24:09 +0900

    add test_post_publish

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,14 @@
+from blog.models import Post
+from django.contrib.auth.models import User
 from django.test import TestCase
+from rest_framework.test import APISimpleTestCase
+import datetime
 
 # Create your tests here.
+class PostTest(TestCase):
+    def test_post_publish(self):
+        admin = User.objects.create(username="hey")
+        post = Post.objects.create(author=admin, title="", text="")
+        assert post.published_date is None
+        post.publish()
+        assert post.published_date < datetime.datetime.now()
