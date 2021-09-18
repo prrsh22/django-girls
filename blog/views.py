df4ec9fd8a0e42e369892344d1c7d7222335bfd9
Sohyun Kim <prrsh22@gmail.com>
2021-09-18 18:50:00 +0900

    add post serializer, post_list view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import PostSerializer
+from .models import Post
 
 # Create your views here.
+@api_view(['GET'])
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    serializer = PostSerializer
+    posts = Post.objects.all()
+    return Response(serializer(instance=posts, many=True).data)
