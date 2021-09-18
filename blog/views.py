2bec796f86b43e4c5e4de0aa103721a68846505a
Sohyun Kim <prrsh22@gmail.com>
2021-09-18 19:02:02 +0900

    add post_detail view(GET)

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import PostSerializer
 from .models import Post
 
@@ -9,3 +10,12 @@ def post_list(request):
     serializer = PostSerializer
     posts = Post.objects.all()
     return Response(serializer(instance=posts, many=True).data)
+
+@api_view(['GET'])
+def post_detail(request, pk):
+    serializer = PostSerializer
+    post = Post.objects.filter(pk=pk).first() #이거 별론가?
+    if post:
+        return Response(serializer(post).data)
+    else:
+        return Response({'message': 'no matching post'}, status=status.HTTP_404_NOT_FOUND)
