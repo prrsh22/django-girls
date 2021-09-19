aa6f46109b18a5c374d69b63652334d2e3277760
Sohyun Kim <prrsh22@gmail.com>
2021-09-19 11:25:11 +0900

    add post_detail(DELETE)

--- blog/views.py
+++ blog/views.py
@@ -11,11 +11,14 @@ def post_list(request):
     posts = Post.objects.all()
     return Response(serializer(instance=posts, many=True).data)
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def post_detail(request, pk):
     serializer = PostSerializer
     post = Post.objects.filter(pk=pk).first() #이거 별론가?
     if post:
+        if (request.method == 'DELETE'):
+            post.delete()
+            return Response({'message': 'deleted'})
         if (request.method == 'PUT'):
             post.title = request.data.get('title') if request.data.get('title') is not None else post.title
             post.text = request.data.get('text') if request.data.get('text') is not None else post.text
