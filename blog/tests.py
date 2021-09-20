14f9d7aeaf99e7c0ef5aa9beb610d32c1272b61c
Sohyun Kim <prrsh22@gmail.com>
2021-09-20 17:30:58 +0900

    add test code for post_detail(GET)

--- blog/tests.py
+++ blog/tests.py
@@ -32,5 +32,23 @@ class PostTest(TestCase):
         assert created_post.status_code == 201
         assert post_number_after_create == post_number_before_create + 1
     
+    def test_GET_not_existing_post_return_404(self):
+        #given
+        number_of_posts = len(Post.objects.all())
+
+        #when
+        response = self.client.get(f'/post/{number_of_posts+1}')
+
+        #then
+        assert response.status_code == 404
     
-        
+    def test_GET_existing_post_return_200(self):
+        #given
+        author = User.objects.create(username="hey")
+        post = Post.objects.create(author=author, title="", text="")
+
+        #when
+        response = self.client.get(f'/post/{post.id}')
+
+        #then
+        assert response.status_code == 200
